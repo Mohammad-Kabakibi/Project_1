52a5ab49e082a4ded0a37577eee8f2a4b2ea499c
Abdeljabbar Elassali <115737234+asly-97@users.noreply.github.com>
2024-07-25 20:33:45 -0400

    setup Reimbursement model

--- src/main/java/com/revature/Project_1/model/Reimbursement.java
+++ src/main/java/com/revature/Project_1/model/Reimbursement.java
@@ -1,4 +1,76 @@
 package com.revature.Project_1.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Reimbursement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int reimbId;
+
+    private String description;
+
+    private float amount;
+
+    private String status;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "userId")
+    private User user;
+
+
+    //boilerplate code
+
+
+    public Reimbursement() {
+    }
+
+    public Reimbursement(int reimbId, String description, float amount, String status, User user) {
+        this.reimbId = reimbId;
+        this.description = description;
+        this.amount = amount;
+        this.status = status;
+        this.user = user;
+    }
+
+    public int getReimbId() {
+        return reimbId;
+    }
+
+    public void setReimbId(int reimbId) {
+        this.reimbId = reimbId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
