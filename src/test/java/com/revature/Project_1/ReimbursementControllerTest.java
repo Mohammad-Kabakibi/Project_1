9c7645fba94e23f8d4b0bff6938aed1b277586b3
Mohammad Kabakibi <Mohammad.2001k@hotmail.com>
2024-07-31 23:36:56 -0400

    added reimbursements tests

---
+++ src/test/java/com/revature/Project_1/ReimbursementControllerTest.java
@@ -0,0 +1,260 @@
+package com.revature.Project_1;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.Project_1.DAO.UserDAO;
+import com.revature.Project_1.JWT.JwtSecurityConfiguration;
+import com.revature.Project_1.model.DTO.IncomingReimbDTO;
+import com.revature.Project_1.model.Reimbursement;
+import com.revature.Project_1.model.Role;
+import com.revature.Project_1.model.User;
+import com.revature.Project_1.service.ReimbursementService;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class ReimbursementControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    ReimbursementService reimbursementService;
+
+    @MockBean
+    UserDAO userDAO;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    public void testCreateReimbursement() throws Exception {
+        IncomingReimbDTO incomingReimbDTO = new IncomingReimbDTO("some description", 100);
+
+        when(reimbursementService.createReimbursement(incomingReimbDTO,"uname")).thenReturn(new Reimbursement());
+
+        String token = get_token("Employee");
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/reimbursements")
+                        .header("Authorization","Bearer "+token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(incomingReimbDTO)))
+                .andExpect(status().isCreated());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/reimbursements")
+                        .header("Authorization","Bearer "+token+"123") // wrong token
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(incomingReimbDTO)))
+                .andExpect(status().isUnauthorized());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/reimbursements")
+                        // without a token
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(incomingReimbDTO)))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void testGetAllReimbursements() throws Exception {
+        var list = Arrays.asList(new Reimbursement(), new Reimbursement());
+        when(reimbursementService.getAllReimbursements()).thenReturn(list);
+        when(reimbursementService.getLoggedInUserReimbursements("uname")).thenReturn(list);
+
+        String token = get_token("Employee");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isOk());
+
+        verify(reimbursementService,times(1)).getLoggedInUserReimbursements("uname");
+
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isOk());
+
+        verify(reimbursementService,times(1)).getAllReimbursements();
+    }
+
+    @Test
+    public void testGetPendingReimbursements() throws Exception {
+        var list = Arrays.asList(new Reimbursement(), new Reimbursement());
+        when(reimbursementService.getPendingReimbursements()).thenReturn(list);
+        when(reimbursementService.getLoggedInUserPendingReimbursements("uname")).thenReturn(list);
+
+        String token = get_token("Employee");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/pending")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isOk());
+
+        verify(reimbursementService,times(1)).getLoggedInUserPendingReimbursements("uname");
+
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/pending")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isOk());
+
+        verify(reimbursementService,times(1)).getPendingReimbursements();
+    }
+
+    @Test
+    public void testUpdateReimbursement() throws Exception {
+        var reimbursement = new Reimbursement();
+        var map = new HashMap<String, String>();
+        when(reimbursementService.updateReimbursementById(anyInt(),any(HashMap.class),anyBoolean(),anyString())).thenReturn(reimbursement);
+
+        String token = get_token("Employee");
+        mockMvc.perform(MockMvcRequestBuilders.patch("/reimbursements/1")
+                        .header("Authorization","Bearer "+token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(map)))
+                .andExpect(status().isOk());
+
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.patch("/reimbursements/1")
+                        .header("Authorization","Bearer "+token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(map)))
+                .andExpect(status().isOk());
+
+        // id as text instead of number
+        mockMvc.perform(MockMvcRequestBuilders.patch("/reimbursements/text")
+                        .header("Authorization","Bearer "+token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(map)))
+                .andExpect(status().isBadRequest());
+
+        verify(reimbursementService,times(2)).updateReimbursementById(anyInt(),any(HashMap.class),anyBoolean(),anyString());
+    }
+
+    @Test
+    public void testGetReimbursementsResolvedAfter() throws Exception {
+        var list = Arrays.asList(new Reimbursement(), new Reimbursement());
+        when(reimbursementService.getReimbursementsResolvedAfter(anyString(),anyBoolean(),anyString())).thenReturn(list);
+
+        String token = get_token("Employee"); // forbidden
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/after/2020-10-10")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isForbidden());
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/after/2020-10-10")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isOk());
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/after/2020-10-10?by_me=true")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isOk());
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/after/2020-10-10?by_me=trueeee")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isBadRequest());
+
+        //by_me = false (default)
+        verify(reimbursementService,times(1)).getReimbursementsResolvedAfter("2020-10-10",false,"uname");
+        //by_me = true
+        verify(reimbursementService,times(1)).getReimbursementsResolvedAfter("2020-10-10",true,"uname");
+    }
+
+    @Test
+    public void testGetReimbursementsResolvedBefore() throws Exception {
+        var list = Arrays.asList(new Reimbursement(), new Reimbursement());
+        when(reimbursementService.getReimbursementsResolvedBefore(anyString(),anyBoolean(),anyString())).thenReturn(list);
+
+        String token = get_token("Employee"); // forbidden
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/before/2020-10-10")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isForbidden());
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/before/2020-10-10")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isOk());
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/before/2020-10-10?by_me=true")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isOk());
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/before/2020-10-10?by_me=trueeee")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isBadRequest());
+
+        //by_me = false (default)
+        verify(reimbursementService,times(1)).getReimbursementsResolvedBefore("2020-10-10",false,"uname");
+        //by_me = true
+        verify(reimbursementService,times(1)).getReimbursementsResolvedBefore("2020-10-10",true,"uname");
+    }
+
+    @Test
+    public void testGetReimbursementsResolvedBetween() throws Exception {
+        var list = Arrays.asList(new Reimbursement(), new Reimbursement());
+        when(reimbursementService.getReimbursementsResolvedBetween(anyString(),anyString(),anyBoolean(),anyString())).thenReturn(list);
+
+        String token = get_token("Employee"); // forbidden
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/between/2020-10-10/2021-10-10")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isForbidden());
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/between/2020-10-10/2021-10-10")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isOk());
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/between/2020-10-10/2021-10-10?by_me=true")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isOk());
+
+        token = get_token("Manager");
+        mockMvc.perform(MockMvcRequestBuilders.get("/reimbursements/resolved/between/2020-10-10/2021-10-10?by_me=trueeee")
+                        .header("Authorization","Bearer "+token))
+                .andExpect(status().isBadRequest());
+
+        //by_me = false (default)
+        verify(reimbursementService,times(1)).getReimbursementsResolvedBetween("2020-10-10","2021-10-10",false,"uname");
+        //by_me = true
+        verify(reimbursementService,times(1)).getReimbursementsResolvedBetween("2020-10-10","2021-10-10",true,"uname");
+    }
+
+
+
+    public String get_token(String role) throws Exception {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("username", "uname");
+        map.put("password", "12345");
+
+        User user = new User(1, "fname","lname","uname",null,null);
+        user.setPassword(JwtSecurityConfiguration.passwordEncoder().encode("12345"));
+        user.setRole(new Role(1,role));
+
+        when(userDAO.findByUsername(anyString())).thenReturn(Optional.of(user));
+
+        var result = mockMvc.perform(MockMvcRequestBuilders.post("/authenticate")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(map)))
+                .andExpect(status().isOk()).andReturn();
+        return new JSONObject(result.getResponse().getContentAsString()).get("token").toString();
+    }
+}
